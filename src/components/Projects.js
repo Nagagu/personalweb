bf1bd042527c923b1e7e0b11e5b7bab0ee77f264
Natalia <n.garridoagudo@gmail.com>
2021-09-21 13:36:55 +0200

    target blank links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,11 @@ export const Projects = () => {
     <div className="projects">
       <div className="section-title">Projects :</div>
       <div className="project-card">
-        <a href="https://nagagu.github.io/TodoList/" className="">
+        <a
+          href="https://nagagu.github.io/TodoList/"
+          className=""
+          target="_blank"
+        >
           <img src={todoAppCapture} alt="Todo List Capture" />
         </a>
 
@@ -21,15 +25,27 @@ export const Projects = () => {
           Simple To-Do application of React js together with the server part
           with C # in .Net environment and Entity Framework
         </p>
-        <a href="https://github.com/Nagagu/TodoList/" className="btn-visit">
+        <a
+          href="https://github.com/Nagagu/TodoList/"
+          className="btn-visit"
+          target="_blank"
+        >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
-        <a href="https://nagagu.github.io/TodoList/" className="btn-visit">
+        <a
+          href="https://nagagu.github.io/TodoList/"
+          className="btn-visit"
+          target="_blank"
+        >
           Visit site
         </a>
       </div>
       <div className="project-card">
-        <a href="https://github.com/Nagagu/TodoListServer/" className="">
+        <a
+          href="https://github.com/Nagagu/TodoListServer/"
+          className=""
+          target="_blank"
+        >
           <img src={swaggerCapture} alt="Todo List Server Capture" />
         </a>
 
@@ -38,13 +54,14 @@ export const Projects = () => {
         <a
           href="https://github.com/Nagagu/TodoListServer/"
           className="btn-visit"
+          target="_blank"
         >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
       </div>
       <div className="project-card">
         <a href="https://nagagu.github.io/portfolio/" className="">
-          <img src={portfolioCapture} alt="portfolio Capture" />
+          <img src={portfolioCapture} alt="portfolio Capture" target="_blank" />
         </a>
 
         <h3>Portfolio example</h3>
@@ -52,15 +69,27 @@ export const Projects = () => {
           I wanted to do something with my photography portfolio and here it is.
           A showcase of some of my best photos and their location.
         </p>
-        <a href="https://github.com/Nagagu/portfolio" className="btn-visit">
+        <a
+          href="https://github.com/Nagagu/portfolio"
+          className="btn-visit"
+          target="_blank"
+        >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
-        <a href="https://nagagu.github.io/portfolio/" className="btn-visit">
+        <a
+          href="https://nagagu.github.io/portfolio/"
+          className="btn-visit"
+          target="_blank"
+        >
           Visit site
         </a>
       </div>
       <div className="project-card">
-        <a href="https://nagagu.github.io/TheNewStore/" className="">
+        <a
+          href="https://nagagu.github.io/TheNewStore/"
+          className=""
+          target="_blank"
+        >
           <img src={thenewstoreCapture} alt="the new store Capture" />
         </a>
 
@@ -69,15 +98,27 @@ export const Projects = () => {
           A simple store in which to add and remove products from the cart using
           Redux and calling an external API
         </p>
-        <a href="https://github.com/Nagagu/TheNewStore" className="btn-visit">
+        <a
+          href="https://github.com/Nagagu/TheNewStore"
+          className="btn-visit"
+          target="_blank"
+        >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
-        <a href="https://nagagu.github.io/TheNewStore/" className="btn-visit">
+        <a
+          href="https://nagagu.github.io/TheNewStore/"
+          className="btn-visit"
+          target="_blank"
+        >
           Visit site
         </a>
       </div>
       <div className="project-card">
-        <a href="https://nagagu.github.io/BattleShipGame/" className="">
+        <a
+          href="https://nagagu.github.io/BattleShipGame/"
+          className=""
+          target="_blank"
+        >
           <img src={battleshipCapture} alt="battleship Capture" />
         </a>
 
@@ -89,19 +130,29 @@ export const Projects = () => {
         <a
           href="https://github.com/Nagagu/BattleShipGame"
           className="btn-visit"
+          target="_blank"
         >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
         <a
           href="https://nagagu.github.io/BattleShipGame/"
           className="btn-visit"
+          target="_blank"
         >
           Visit site
         </a>
       </div>
       <div className="project-card">
-        <a href="https://nagagu.github.io/catsApp/" className="">
-          <a href="https://nagagu.github.io/catsApp/" className="">
+        <a
+          href="https://nagagu.github.io/catsApp/"
+          className=""
+          target="_blank"
+        >
+          <a
+            href="https://nagagu.github.io/catsApp/"
+            className=""
+            target="_blank"
+          >
             <img src={catQuizCapture} alt="cat Quiz Capture" />
           </a>
         </a>
@@ -110,21 +161,33 @@ export const Projects = () => {
           Answer the questions to find out how much of a geek you are. Also
           developed as PWA.
         </p>
-        <a href="https://github.com/Nagagu/catsApp/" className="btn-visit">
+        <a
+          href="https://github.com/Nagagu/catsApp/"
+          className="btn-visit"
+          target="_blank"
+        >
           Github <i class="fab fa-github fa-lg"></i>
         </a>
-        <a href="https://nagagu.github.io/catsApp/" className="btn-visit">
+        <a
+          href="https://nagagu.github.io/catsApp/"
+          className="btn-visit"
+          target="_blank"
+        >
           Visit site
         </a>
       </div>
       <div className="project-card">
-        <a href="https://creandovinculos.com/" className="">
+        <a href="https://creandovinculos.com/" className="" target="_blank">
           <img src={creandovinculos} alt="creando vinculos Capture" />
         </a>
 
         <h3>Creando Vínculos Wordpress</h3>
         <p>A wordpress created with care for a psychotherapy professional</p>
-        <a href="https://creandovinculos.com/" className="btn-visit">
+        <a
+          href="https://creandovinculos.com/"
+          className="btn-visit"
+          target="_blank"
+        >
           Visit site
         </a>
       </div>
